654645062115094b85d75044f9a608b7412b2823
vicenzodev <heitor@vicenzo.dev>
2025-11-24 22:51:13 -0300

    sistema de reset evolução

--- src/app/api/reset/route.ts
+++ src/app/api/reset/route.ts
@@ -1,4 +1,29 @@
- 
-export const GET = async () =>{
+import { getUsSenha, updateUsSenha, verifyExpDate } from '@/services/us-service';
+import pass from 'generate-password';
+import { getUserSession } from "@/services/auth";
+import { NextResponse, NextRequest } from "next/server";
 
+export const GET = async (req:NextRequest) =>{
+    
+    try{
+        const id = await getUserSession();
+        if(!id) throw new Error("Não autorizado");
+
+        const usersToReset = await verifyExpDate();
+        for(const user of usersToReset){
+            const senha = pass.generate({
+                length: 16,
+                numbers: true,
+                symbols: true,
+                uppercase: true,
+                excludeSimilarCharacters: true,
+                strict: true
+            });
+            const lastSenha = await getUsSenha(id.id,user.id);
+            updateUsSenha(id.id,user.id,senha,lastSenha);
+        }
+        return NextResponse.json({message:'Senhas expiradas alteradas com sucesso'},{status:200});
+    }catch(error){
+        
+    }
 }
--- src/services/us-service.ts
+++ src/services/us-service.ts
@@ -1,4 +1,4 @@
-import { criptografarSenha } from "@/utils/crypto";
+import { criptografarSenha, descriptografarSenha } from "@/utils/crypto";
 import { prisma } from "@/utils/prisma"
 interface IUs{
     sistema:string,
@@ -29,8 +29,44 @@ export const createUs = async (data:IUs): Promise<Omit<IUs,'senhas'>> =>{
     return result;
 }
 
-export const getUs = async (utaId:number): Promise<IUs[]> =>{
+export const getUs = async (utaId:number) =>{
     const us = await prisma.us.findMany({where:{utaId:utaId}});
-    if(!us) throw new Error("Nenhum usuário de sistema encontrado");
+    if(!us) throw new Error("Nenhum usuário de sistema encontrado :(");
+    let usNoPassword = [];
+    for(const data of us){
+        const {senhas, ...result} = data;
+        usNoPassword.push(result);
+    }
+    return usNoPassword;
+}
+
+export const getUsSenha = async (utaId:number,usId:number)=>{
+    const us = await prisma.us.findFirst({where:{id:usId,utaId:utaId}});
+    if(!us) return '';
+    const senha = await descriptografarSenha(us.senhas);
+    return senha;
+}
+
+export const updateUsSenha = async (utaId:number,usId:number,senha:string,lastSenha:string) =>{
+    const us = await prisma.us.update({
+        where:{
+            id:usId,
+            utaId:utaId
+        },
+        data:{
+            senhas: criptografarSenha(senha),
+            last_senha: criptografarSenha(lastSenha)
+        }
+    });
+    return us;
+}
+
+export const verifyExpDate = async () =>{
+    const us = prisma.us.findMany({
+        where:{
+            exp_date:{lt:new Date()}
+        }
+    });
+    if(!us) throw new Error("Nenhum usuário de sistema encontrado :(");
     return us;
 }
